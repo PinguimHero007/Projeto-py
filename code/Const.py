9ab61e17cb905bdfdea88890dd50deb7ab9e6c00
Pinguim007 <kaiogouveia06@gmail.com>
2025-08-28 13:25:28 -0300

    Creating main loop Connecting Menu and Game classes

---
+++ code/Const.py
@@ -0,0 +1,15 @@
+# C
+C_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 0)
+
+# M
+MENU_OPTION = ('NOVO JOGO 1P',
+               'NOVO JOGO 2P - COOP',
+               'NOVO JOGO 2P - VS',
+               'PONTUAÇÃO'
+               'SAIR',)
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
